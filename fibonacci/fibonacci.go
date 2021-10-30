4e62a5122df92ce29a1cdc8a079feabe7c17dda7
John Cairns <john@2ad.com>
2021-10-30 11:02:53 -0500

    untabify

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -1,77 +1,77 @@
 package fibonacci
 
 import (
-	"math/big"
+    "math/big"
 )
 
 type Fibonacci interface {
-	Fib(n int) *big.Int
+    Fib(n int) *big.Int
 }
 
 type Calculator struct {
-	fibn map[int]*big.Int
+    fibn map[int]*big.Int
 }
 
 func (calc Calculator) Fib(n int) *big.Int {
-	n0, _ := fib_fast(n, calc.fibn)
-	return n0
+    n0, _ := fib_fast(n, calc.fibn)
+    return n0
 }
 
 func NewPrecompute(size int) Fibonacci {
-	calc := new(Calculator)
-	calc.fibn = make(map[int]*big.Int)
-	for i := 0; i < size; i++ {
-		ni, _ := fib(i)
-		calc.fibn[i] = ni
-	}
-	return calc
+    calc := new(Calculator)
+    calc.fibn = make(map[int]*big.Int)
+    for i := 0; i < size; i++ {
+        ni, _ := fib(i)
+        calc.fibn[i] = ni
+    }
+    return calc
 }
 
 func NewCalc() Fibonacci {
-	calc := new(Calculator)
-	calc.fibn = make(map[int]*big.Int)
-	return calc
+    calc := new(Calculator)
+    calc.fibn = make(map[int]*big.Int)
+    return calc
 }
 
 func fast_doubler(n int, a *big.Int, b *big.Int) (*big.Int, *big.Int) {
-	var cx big.Int
-	cx.Mul(b, big.NewInt(2))
-	var b2a big.Int
-	b2a.Sub(&cx, a)
-	cx.Mul(a, &b2a)
-	var bsq big.Int
-	bsq.Mul(b, b)
-	var asq big.Int
-	asq.Mul(a, a)
-	var d big.Int
-	d.Add(&asq, &bsq)
-	if n%2 == 0 {
-		return &cx, &d
-	} else {
-		cx.Add(&cx, &d)
-		return &d, &cx
-	}
+    var cx big.Int
+    cx.Mul(b, big.NewInt(2))
+    var b2a big.Int
+    b2a.Sub(&cx, a)
+    cx.Mul(a, &b2a)
+    var bsq big.Int
+    bsq.Mul(b, b)
+    var asq big.Int
+    asq.Mul(a, a)
+    var d big.Int
+    d.Add(&asq, &bsq)
+    if n%2 == 0 {
+        return &cx, &d
+    } else {
+        cx.Add(&cx, &d)
+        return &d, &cx
+    }
 }
 
 func fib_fast(n int, fibn map[int]*big.Int) (*big.Int, *big.Int) {
-	value, containsKey := fibn[n+1]
-	if containsKey {
-		return fibn[n], value
-	} else if n == 0 {
-		n0, n1 := big.NewInt(0), big.NewInt(1)
-		return n0, n1
-	} else {
-		a, b := fib_fast(n/2, fibn)
-		return fast_doubler(n, a, b)
-	}
+    value, containsKey := fibn[n+1]
+    if containsKey {
+        return fibn[n], value
+    } else if n == 0 {
+        n0, n1 := big.NewInt(0), big.NewInt(1)
+        return n0, n1
+    } else {
+        a, b := fib_fast(n/2, fibn)
+        return fast_doubler(n, a, b)
+    }
 }
 
 func fib(n int) (*big.Int, *big.Int) {
-	if n == 0 {
-		n0, n1 := big.NewInt(0), big.NewInt(1)
-		return n0, n1
-	} else {
-		a, b := fib(n / 2)
-		return fast_doubler(n, a, b)
-	}
+    if n == 0 {
+        n0, n1 := big.NewInt(0), big.NewInt(1)
+        return n0, n1
+    } else {
+        a, b := fib(n / 2)
+        return fast_doubler(n, a, b)
+    }
 }
--- fibonacci/fibonacci_test.go
+++ fibonacci/fibonacci_test.go
@@ -1,159 +1,159 @@
 package fibonacci
 
 import (
-	"math/big"
-	"reflect"
-	"testing"
+    "math/big"
+    "reflect"
+    "testing"
 )
 
 func TestCalculator_Fib(t *testing.T) {
-	type fields struct {
-		fibn map[int]*big.Int
-	}
-	type args struct {
-		n int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   *big.Int
-	}{
-		{
-			name:   "ZeroCero",
-			fields: fields{make(map[int]*big.Int)},
-			args:   args{0},
-			want:   big.NewInt(0),
-		},
-		{
-			name:   "OneUne",
-			fields: fields{make(map[int]*big.Int)},
-			args:   args{1},
-			want:   big.NewInt(1),
-		},
-		{
-			name:   "SimpleExample13",
-			fields: fields{make(map[int]*big.Int)},
-			args:   args{13},
-			want:   big.NewInt(233),
-		},
-		{
-			name:   "SimpleExample84",
-			fields: fields{make(map[int]*big.Int)},
-			args:   args{84},
-			want:   big.NewInt(160500643816367088),
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			calc := Calculator{
-				fibn: tt.fields.fibn,
-			}
-			if got := calc.Fib(tt.args.n); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Calculator.Fib() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+    type fields struct {
+        fibn map[int]*big.Int
+    }
+    type args struct {
+        n int
+    }
+    tests := []struct {
+        name   string
+        fields fields
+        args   args
+        want   *big.Int
+    }{
+        {
+            name:   "ZeroCero",
+            fields: fields{make(map[int]*big.Int)},
+            args:   args{0},
+            want:   big.NewInt(0),
+        },
+        {
+            name:   "OneUne",
+            fields: fields{make(map[int]*big.Int)},
+            args:   args{1},
+            want:   big.NewInt(1),
+        },
+        {
+            name:   "SimpleExample13",
+            fields: fields{make(map[int]*big.Int)},
+            args:   args{13},
+            want:   big.NewInt(233),
+        },
+        {
+            name:   "SimpleExample84",
+            fields: fields{make(map[int]*big.Int)},
+            args:   args{84},
+            want:   big.NewInt(160500643816367088),
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            calc := Calculator{
+                fibn: tt.fields.fibn,
+            }
+            if got := calc.Fib(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+                t.Errorf("Calculator.Fib() = %v, want %v", got, tt.want)
+            }
+        })
+    }
 }
 
 func TestNewCalc(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		{
-			name: "CreateOne",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewCalc(); got == nil {
-				t.Errorf("NewCalc() = %v, want not nil", got)
-			}
-		})
-	}
+    tests := []struct {
+        name string
+    }{
+        {
+            name: "CreateOne",
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            if got := NewCalc(); got == nil {
+                t.Errorf("NewCalc() = %v, want not nil", got)
+            }
+        })
+    }
 }
 
 func Test_fib_fast(t *testing.T) {
-	type args struct {
-		n    int
-		fibn map[int]*big.Int
-	}
-	tests := []struct {
-		name  string
-		args  args
-		want  *big.Int
-		want1 *big.Int
-	}{
-		{
-			name:  "Fast0",
-			args:  args{0, make(map[int]*big.Int)},
-			want:  big.NewInt(0),
-			want1: big.NewInt(1),
-		},
-		{
-			name:  "Fast1",
-			args:  args{1, make(map[int]*big.Int)},
-			want:  big.NewInt(1),
-			want1: big.NewInt(1),
-		},
-		{
-			name:  "Fast13",
-			args:  args{13, make(map[int]*big.Int)},
-			want:  big.NewInt(233),
-			want1: big.NewInt(377),
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := fib_fast(tt.args.n, tt.args.fibn)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("fib_fast() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("fib_fast() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
+    type args struct {
+        n    int
+        fibn map[int]*big.Int
+    }
+    tests := []struct {
+        name  string
+        args  args
+        want  *big.Int
+        want1 *big.Int
+    }{
+        {
+            name:  "Fast0",
+            args:  args{0, make(map[int]*big.Int)},
+            want:  big.NewInt(0),
+            want1: big.NewInt(1),
+        },
+        {
+            name:  "Fast1",
+            args:  args{1, make(map[int]*big.Int)},
+            want:  big.NewInt(1),
+            want1: big.NewInt(1),
+        },
+        {
+            name:  "Fast13",
+            args:  args{13, make(map[int]*big.Int)},
+            want:  big.NewInt(233),
+            want1: big.NewInt(377),
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            got, got1 := fib_fast(tt.args.n, tt.args.fibn)
+            if !reflect.DeepEqual(got, tt.want) {
+                t.Errorf("fib_fast() got = %v, want %v", got, tt.want)
+            }
+            if !reflect.DeepEqual(got1, tt.want1) {
+                t.Errorf("fib_fast() got1 = %v, want %v", got1, tt.want1)
+            }
+        })
+    }
 }
 
 func Test_fib(t *testing.T) {
-	type args struct {
-		n int
-	}
-	tests := []struct {
-		name  string
-		args  args
-		want  *big.Int
-		want1 *big.Int
-	}{
-		{
-			name:  "fib_zero",
-			args:  args{0},
-			want:  big.NewInt(0),
-			want1: big.NewInt(1),
-		},
-		{
-			name:  "fib_one",
-			args:  args{1},
-			want:  big.NewInt(1),
-			want1: big.NewInt(1),
-		},
-		{
-			name:  "fib_exact100",
-			args:  args{90},
-			want:  big.NewInt(2880067194370816120),
-			want1: big.NewInt(4660046610375530309),
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := fib(tt.args.n)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("fib() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("fib() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
+    type args struct {
+        n int
+    }
+    tests := []struct {
+        name  string
+        args  args
+        want  *big.Int
+        want1 *big.Int
+    }{
+        {
+            name:  "fib_zero",
+            args:  args{0},
+            want:  big.NewInt(0),
+            want1: big.NewInt(1),
+        },
+        {
+            name:  "fib_one",
+            args:  args{1},
+            want:  big.NewInt(1),
+            want1: big.NewInt(1),
+        },
+        {
+            name:  "fib_exact100",
+            args:  args{90},
+            want:  big.NewInt(2880067194370816120),
+            want1: big.NewInt(4660046610375530309),
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            got, got1 := fib(tt.args.n)
+            if !reflect.DeepEqual(got, tt.want) {
+                t.Errorf("fib() got = %v, want %v", got, tt.want)
+            }
+            if !reflect.DeepEqual(got1, tt.want1) {
+                t.Errorf("fib() got1 = %v, want %v", got1, tt.want1)
+            }
+        })
+    }
 }
